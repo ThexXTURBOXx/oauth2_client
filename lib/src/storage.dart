91025b6ae0a4ed1396f390e90dcbd5aa6d535fe6
Nico Mexis <nico.mexis@kabelmail.de>
2026-08-12 17:05:56 +0200

    Fix/update storage documentation

--- lib/src/storage.dart
+++ lib/src/storage.dart
@@ -1,5 +1,6 @@
 import 'base_storage.dart';
 
-/// Implemented in `browser_client.dart` and `io_client.dart`.
+/// Implemented in `token_storage.dart` and `secure_storage.dart`.
+/// Alternatively, a non-persistent implementation is available in `volatile_storage.dart`.
 BaseStorage createStorage() => throw UnsupportedError(
-    'Cannot create a storage without dart:html or dart:io.');
+    'Cannot create a storage without dart:js_interop or dart:io.');
